7a3eca85aa9dae5bbd2ecf390a8a52e7b88b5322
kmanc <kmanc@comcast.net>
2021-12-08 09:54:23 -0800

    The solution isn't intuitive, so here are some comments

--- 2021/chal8/2solve.py
+++ 2021/chal8/2solve.py
@@ -20,32 +20,42 @@ for line in lines:
     scramble_unscramble_dict = {}
 
     seed_string = line[0]
+    # Seed inputs sorted shortest --> longest
+    seeds = sorted(seed_string.split(" "), key=len)
+    # Seed inputs as one long string without spaces
+    seed_string = seed_string.replace(" ", "")
 
-    b_map = {letter for letter in seed_string if seed_string.count(letter) == 6 and letter != " "}.pop()
+    # The letter b shows up in 6 of the displays
+    b_map = {letter for letter in seed_string if seed_string.count(letter) == 6}.pop()
     scramble_unscramble_dict[b_map] = "b"
 
-    e_map = {letter for letter in seed_string if seed_string.count(letter) == 4 and letter != " "}.pop()
+   # The letter e shows up in 4 of the displays
+    e_map = {letter for letter in seed_string if seed_string.count(letter) == 4}.pop()
     scramble_unscramble_dict[e_map] = "e"
 
-    f_map = {letter for letter in seed_string if seed_string.count(letter) == 9 and letter != " "}.pop()
+    # The letter f shows up in 9 of the displays
+    f_map = {letter for letter in seed_string if seed_string.count(letter) == 9}.pop()
     scramble_unscramble_dict[f_map] = "f"
 
-    seeds = sorted(seed_string.split(" "), key=len)
-
+    # Since the shortest seed input has f + c and we already know f, we know c
     c_map = seeds[0].replace(f_map, "")
     scramble_unscramble_dict[c_map] = "c"
 
+    # Like above, the second shortest input has f + c + a and we already know f + c
     a_map = seeds[1].replace(c_map, "").replace(f_map, '')
     scramble_unscramble_dict[a_map] = "a"
 
+    # Of the unsolved letters left (d + g), g shows up 3 times in the seeds that have 6 segments
     six_len = "".join(entry for entry in seeds if len(entry) == 6)
     six_len = "".join(letter for letter in six_len if letter not in scramble_unscramble_dict)
     g_map = {letter for letter in seed_string if six_len.count(letter) == 3}.pop()
     scramble_unscramble_dict[g_map] = "g"
 
+    # Like above, d shows up only twice in the seeds that have 6 segments
     d_map = {letter for letter in seed_string if six_len.count(letter) == 2}.pop()
     scramble_unscramble_dict[d_map] = "d"
 
+    # Remap the scrambled letters to the correct ones, reconstruct the string and sort it alphabetically so I can map it to its real number via the dict outside this loop
     real_values = line[1].split(" ")
     mini_total = 0
     for real_value in real_values:
