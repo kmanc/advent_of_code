2c29c2ed50347aad04c4d939716d1aa71092d223
kmanc <kmanc@comcast.net>
2021-12-08 10:22:06 -0800

    collections.Counter is cooler

--- 2021/chal8/2solve.py
+++ 2021/chal8/2solve.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 with open("in.txt", "r") as f:
     lines = [line for line in f.readlines()]
     lines = [(line.split("|")[0].strip(), line.split("|")[1].strip()) for line in lines]
@@ -22,19 +24,21 @@ for line in lines:
     seed_string = line[0]
     # Seed inputs sorted shortest --> longest
     seeds = sorted(seed_string.split(" "), key=len)
-    # Seed inputs as one long string without spaces
-    seed_string = seed_string.replace(" ", "")
+    # Counts of seed letter frequency, no spaces
+    seed_count = Counter(seed_string.replace(" ", ""))
+    # Swap keys and values so we have count --> letter
+    seed_count = {v: k for k, v in seed_count.items()}
 
     # The letter b shows up in 6 of the displays
-    b_map = {letter for letter in seed_string if seed_string.count(letter) == 6}.pop()
+    b_map = seed_count.get(6)
     scramble_unscramble_dict[b_map] = "b"
 
    # The letter e shows up in 4 of the displays
-    e_map = {letter for letter in seed_string if seed_string.count(letter) == 4}.pop()
+    e_map = seed_count.get(4)
     scramble_unscramble_dict[e_map] = "e"
 
     # The letter f shows up in 9 of the displays
-    f_map = {letter for letter in seed_string if seed_string.count(letter) == 9}.pop()
+    f_map = seed_count.get(9)
     scramble_unscramble_dict[f_map] = "f"
 
     # Since the shortest seed input has f + c and we already know f, we know c
@@ -47,12 +51,13 @@ for line in lines:
 
     # Of the unsolved letters left (d + g), g shows up 3 times in the seeds that have 6 segments
     six_len = "".join(entry for entry in seeds if len(entry) == 6)
-    six_len = "".join(letter for letter in six_len if letter not in scramble_unscramble_dict)
-    g_map = {letter for letter in seed_string if six_len.count(letter) == 3}.pop()
+    six_len_count = Counter(letter for letter in six_len if letter not in scramble_unscramble_dict)
+    six_len_count = {v: k for k, v in six_len_count.items()}
+    g_map = six_len_count.get(3)
     scramble_unscramble_dict[g_map] = "g"
 
     # Like above, d shows up only twice in the seeds that have 6 segments
-    d_map = {letter for letter in seed_string if six_len.count(letter) == 2}.pop()
+    d_map = six_len_count.get(2)
     scramble_unscramble_dict[d_map] = "d"
 
     # Remap the scrambled letters to the correct ones, reconstruct the string and sort it alphabetically so I can map it to its real number via the dict outside this loop
